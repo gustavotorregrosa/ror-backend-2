250f65c9d050dfc15675e3dfaa643855ebb2c885
Gustavo Torregrosa Costa <gustavo@Gustavos-MacBook-Air.local>
2023-02-26 20:54:17 -0300

    parcial

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "ruby-backend.gustavotorregrosa.link"
+  config.hosts << "rubyonrails-backend.gustavotorregrosa.link"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
